a8a41b49f8f7a9e3aba2574a2891908788227bc7
poojainfinite <pooja.infinite.tech@gmail.com>
2026-05-31 10:05:41 +0530

    Fix: index.html with PWA

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,22 @@
-// Service Worker for Offline Caching & PWA Installability
-const CACHE_NAME = 'city-glide-v1';
-const ASSETS = [
+const CACHE_NAME = 'cityglide-v1';
+const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json',
+  '/icon-192.png',
   '/icon-512.png',
-  '/icon-192.png'
+  '/manifest.json'
 ];
 
-self.addEventListener('install', (event: any) => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (event: any) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
