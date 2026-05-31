71f011241ae4600d14c8e6f2056fefb2cdfac0c4
poojainfinite <poojainfinite@gmail.com>
2026-05-31 06:18:15 +0530

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "./",
+  base: '/cityglide-app/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
